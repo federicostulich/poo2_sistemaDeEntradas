c3ac4c840d2a853165045ae6efa559f38f50266b
Fs <fede.stulich@gmail.com>
2023-10-29 20:58:45 -0300

    Creacion objeto Usuario

---
+++ src/usuario.js
@@ -0,0 +1,10 @@
+function Usuario(nombre,apellido, dni, fechaNacimiento) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.fechaNacimiento = fechaNacimiento;
+
+    
+}
+
+module.exports = Usuario;
---
+++ test/usuario.test.js
@@ -0,0 +1,16 @@
+const Usuario = require("../src/usuario.js")
+
+describe('Gestion de usuarios', () => {
+
+    test('Crear un usuario', ()=> {
+        const usuario = new Usuario("Matias",
+                                    "Gonzalez",
+                                    "35111222",
+                                    new Date(1990, 0, 1))
+        expect(usuario.nombre).toBe("Matias")
+        expect(usuario.apellido).toBe("Gonzalez")
+        expect(usuario.dni).toBe("35111222")
+        expect(usuario.fechaNacimiento).toEqual(new Date(1990, 0, 1))
+    })
+
+})
