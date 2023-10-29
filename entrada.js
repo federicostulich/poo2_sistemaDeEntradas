2909e6f938174d0a1da645f122b16cbb6aa48341
Fs <fede.stulich@gmail.com>
2023-10-29 20:52:57 -0300

    Creacion objeto Entrada

---
+++ entrada.js
@@ -0,0 +1,29 @@
+function Entrada(precio,ubicacion) {
+    this.precio = precio;
+    this.ubicacion = ubicacion;
+    this.estaDisponible = true;
+    this.comprador = null;
+
+    this.comprar = (comprador) => {
+        this.validarDisponibilidad();
+        this.validarComprador(comprador);
+        
+        this.estaDisponible = false;
+        this.comprador = comprador;
+    }
+
+    this.validarDisponibilidad = () => {
+        if(!this.estaDisponible) {
+            throw new Error('No esta disponible')
+        }
+    }
+
+    this.validarComprador = (comprador) => {
+        if(!comprador) {
+            throw new Error("Necesita informacion del comprador")
+        }
+    }
+    
+}
+
+module.exports = Entrada;
