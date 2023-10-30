b4d2f39a4838f16095dd920689d8ba04ebaa3726
Fs <fede.stulich@gmail.com>
2023-10-29 21:43:22 -0300

    Refactor: extract function validarQueNoEsSocio

--- src/usuario.js
+++ src/usuario.js
@@ -11,15 +11,19 @@ function Usuario(nombre,apellido, dni, fechaNacimiento, esSocio = false) {
     }
 
     this.desasociar = () => {
-        if(!this.esSocio){
-            throw new Error("El usuario no es socio.");
-        }
+        this.validarQueNoEsSocio();
         this.esSocio = false;
     }
 
     this.validarQueEsSocio = () => {
         if(this.esSocio){
             throw new Error("El usuario ya es socio.");
+        }
+    }
+
+    this.validarQueNoEsSocio = () => {
+        if(!this.esSocio){
+            throw new Error("El usuario no es socio.");
         }
     }
     
